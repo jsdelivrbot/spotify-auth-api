da1671296017425052ee8c0a03ce8671f86f1324
Noah Lubko <nlubko@gmail.com>
2017-06-03 16:22:59 +0300

    update name

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/', function(request, response) {
-//     response.send('Hello World!')
-// })
-
 var bookRouter = express.Router();
-bookRouter.route('/Books')
+bookRouter.route('/token')
     .get(function(req, res) {
         var resopneJson = { hello: 'this is my api' }
         res.json(resopneJson);
     });
 
-app.use('/api', bookRouter);
+app.use('/', bookRouter);
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
