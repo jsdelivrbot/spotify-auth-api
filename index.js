d8a5fd18e009602e090624a0849f50381ef8f4b8
Noah Lubko <nlubko@gmail.com>
2017-06-03 16:05:06 +0300

    show route

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-    response.send('Hello World1!')
-})
+// app.get('/', function(request, response) {
+//     response.send('Hello World!')
+// })
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+    .get(function(req, res) {
+        var resopneJson = { hello: 'this is my api' }
+        res.json(resopneJson);
+    });
+
+app.use('/api', bookRouter);
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
