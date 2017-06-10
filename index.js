08edebbccd129960c868325fb10fd51ac45a3587
Noah Lubko <nlubko@gmail.com>
2017-06-10 15:05:49 +0300

    add spotify-web-api-node call

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var SpotifyWebApi = require('spotify-web-api-node');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -7,8 +8,22 @@ app.use(express.static(__dirname + '/public'));
 var bookRouter = express.Router();
 bookRouter.route('/token')
     .get(function(req, res) {
-        var resopneJson = { hello: 'this is my api' }
-        res.json(resopneJson);
+        // Set necessary parts of the credentials on the constructor
+        var spotifyApi = new SpotifyWebApi({
+            clientId: 'cbc23ecc93514b9e896057597aa79ce6',
+            clientSecret: 'f34d14b57a574c44886a273ae79ac45f'
+        });
+
+        // Get an access token and 'save' it using a setter
+        spotifyApi.clientCredentialsGrant()
+            .then(function(data) {
+                //console.log('The access token is ' + data.body['access_token']);
+                //spotifyApi.setAccessToken(data.body['access_token']);
+                var resopneJson = { hello: data }
+                res.json(resopneJson);
+            }, function(err) {
+                console.log('Something went wrong!', err);
+            });
     });
 
 app.use('/', bookRouter);
